a1c4bfb3f4e619279d3c7114e777c67da87bc655
池ノ谷優太 <you1207.ike@gmail.com>
2023-02-25 23:25:05 +0900

    First Template Commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+//import testImage from './assets/logo192.png';
+
+function App() {
+  return (
+    <div className="App">
+			{/*ここにコンポーネントを追加していく*/}
+			<p>Hello Manual React</p>
+			{/*<img src={testImage}/>*/}
+    </div>
+  );
+}
+
+export default App;
